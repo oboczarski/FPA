6b0d35000e4f09fb824bc3822eed967ba9308be7
oscarbrandsllc <oscarbrands.llc@gmail.com>
2025-12-17 16:09:39 -0800

    Update NFL team colors; add detailed comments and organize by division for better clarity

--- DH-FPA/app.js
+++ DH-FPA/app.js
@@ -144,17 +144,51 @@ const POS_COLORS = {
   TE: { base: [180, 105, 255], hex: "#B469FF" },  // purple
 };
 
-// NFL Team colors (adjusted for dark theme visibility)
+// NFL Team colors - Official primary colors from teamcolorcodes.com
 const NFL_TEAM_COLORS = {
-  'SF': '#B3995D', 'CHI': '#C83803', 'CIN': '#FB4F14', 'BUF': '#C60C30',
-  'DEN': '#FB4F14', 'CLE': '#FF3C00', 'TB': '#DC4405', 'ARI': '#97233F',
-  'LAC': '#0080C6', 'SD': '#0080C6', 'KC': '#E31837', 'IND': '#3D7EDB',
-  'WAS': '#991515', 'DAL': '#869397', 'MIA': '#008E97', 'PHI': '#4CCC6E',
-  'ATL': '#A71930', 'NYG': '#4169E1', 'JAX': '#00A8A8', 'NYJ': '#1C8F5E',
-  'DET': '#0076B6', 'GB': '#2C8244', 'CAR': '#0085CA', 'NE': '#3C6491',
-  'LV': '#A5ACAF', 'OAK': '#A5ACAF', 'LAR': '#4876D6', 'STL': '#003594',
-  'BAL': '#6B5CD6', 'NO': '#D3BC8D', 'SEA': '#69BE28', 'PIT': '#FFB612',
-  'HOU': '#4080C0', 'TEN': '#4B92DB', 'MIN': '#7B4EC2'
+  // AFC North
+  'BAL': '#241773',  // Ravens Purple
+  'CIN': '#FB4F14',  // Bengals Orange
+  'CLE': '#FF3C00',  // Browns Orange
+  'PIT': '#FFB612',  // Steelers Gold
+  // AFC East
+  'BUF': '#00338D',  // Bills Blue
+  'MIA': '#008E97',  // Dolphins Aqua
+  'NE': '#002244',   // Patriots Nautical Blue
+  'NYJ': '#125740',  // Jets Gotham Green
+  // AFC South
+  'HOU': '#A71930',  // Texans Battle Red
+  'IND': '#002C5F',  // Colts Speed Blue
+  'JAX': '#006778',  // Jaguars Teal
+  'TEN': '#4B92DB',  // Titans Blue
+  // AFC West
+  'DEN': '#FB4F14',  // Broncos Orange
+  'KC': '#E31837',   // Chiefs Red
+  'LV': '#A5ACAF',   // Raiders Silver
+  'OAK': '#A5ACAF',  // Raiders Silver (old)
+  'LAC': '#0080C6',  // Chargers Powder Blue
+  'SD': '#0080C6',   // Chargers (old)
+  // NFC North
+  'CHI': '#C83803',  // Bears Orange
+  'DET': '#0076B6',  // Lions Honolulu Blue
+  'GB': '#203731',   // Packers Dark Green
+  'MIN': '#4F2683',  // Vikings Purple
+  // NFC East
+  'DAL': '#003594',  // Cowboys Royal Blue
+  'NYG': '#0B2265',  // Giants Dark Blue
+  'PHI': '#004C54',  // Eagles Midnight Green
+  'WAS': '#5A1414',  // Commanders Burgundy
+  // NFC South
+  'ATL': '#A71930',  // Falcons Red
+  'CAR': '#0085CA',  // Panthers Carolina Blue
+  'NO': '#D3BC8D',   // Saints Old Gold
+  'TB': '#D50A0A',   // Buccaneers Red
+  // NFC West
+  'ARI': '#97233F',  // Cardinals Red
+  'LAR': '#003594',  // Rams Blue
+  'STL': '#003594',  // Rams (old)
+  'SF': '#AA0000',   // 49ers Red
+  'SEA': '#69BE28',  // Seahawks Action Green
 };
 
 // Points thresholds for conditional formatting
